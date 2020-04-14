930ec54777d6157783453ddf30c8b808334792a0
RichardKim <hiddeneventor@gmail.com>
2020-04-14 17:36:43 +0900

    주소 시도, 시군구, 읍면동, 리 기준으로 유사성이 가장 높은 걸로 변환해주는 프로그램

--- test01/lec01.py
+++ test01/lec01.py
@@ -1,1 +1,34 @@
-test
+from difflib import SequenceMatcher
+import operator
+# from scipy import spatial
+#
+#
+
+dataSetI = ["서울","신도림","대구","대전"]
+dataSetII = ["서울","신도림"]
+intList = {}
+# result = 1 - spatial.distance.cosine(dataSetI, dataSetII)
+# print(result)
+# for x in range(10):
+#     # intList.clear()
+#     print(intList)
+#     for i, v in enumerate(dataSetI):
+#         intList[i] = v
+#
+# print(intList)
+
+# testdic = {'서울': 0.1, '대전': 0.2, '대구':0.5 , '대전':0.2}
+# print(max(testdic.values()))
+
+state = {1:1000, 2:3000, 3: 100, 4:3000}
+result = max(state, key=state.get)
+print(result)
+
+# def similar(a, b):
+#     return SequenceMatcher(None, a, b).ratio()
+# result = similar("서울","서울") #would have a high prob.
+# if result == 1.0 :
+#     print(result)
+#
+# print()
+
