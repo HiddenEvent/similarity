2578cc6c005e0f239f57833ed155ed64adb06cb0
RichardKim <hiddeneventor@gmail.com>
2020-04-15 13:08:22 +0900

    엑셀로 주소데이터 가공 완료

--- test01/similer.py
+++ test01/similer.py
@@ -8,7 +8,7 @@ def similar(a, b):
 search_addr = []
 nosearch_addr = []
 
-########## 시스템적 주소
+########## 시스템DB 주소
 gijun_df = pd.read_csv("./sim_gijun.CSV", error_bad_lines=False, index_col = False)
 gijun_list = gijun_df.fillna("").values
 gijun_join_list = ['_'.join(row) for row in gijun_list]
@@ -27,9 +27,14 @@ def searchAddr(addr):
 
 def nosearch_decipher(se_index, similarList ) :
     similer_index = max(similarList, key=similarList.get)
-    # 조회할 데이터 
-    noaddrString = '( '+str(se_index)+'번 )'+se_join_list[se_index]+" => "+gijun_join_list[similer_index]
-    nosearch_addr.append(noaddrString)
+    # 조회할 데이터
+    index = se_index
+    se_sido, se_sgg, se_emd = se_join_list[se_index].split('_')
+    changeTitle = " <= DB 데이터 변경"
+    sim_sido, sim_sgg, sim_emd, sim_li = gijun_join_list[similer_index].split('_')
+
+    # noaddrString = '( '+str(se_index)+'번 )'+se_join_list[se_index]+" => "+gijun_join_list[similer_index]
+    nosearch_addr.append([index, sim_sido, sim_sgg, sim_emd, sim_li, changeTitle,se_sido, se_sgg, se_emd])
     
 ########################
 gijun_len = len(gijun_join_list)-1
